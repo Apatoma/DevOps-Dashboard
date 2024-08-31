40e42be20baea6c816d6cb1378708d4bcf975e40
Sury <134803136+Apatoma@users.noreply.github.com>
2024-08-31 20:51:33 +0200

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-JENKINS_URL = 'http://your-jenkins-url.com'
-JENKINS_USER = 'your-username'
-JENKINS_TOKEN = 'your-api-token'
-
-DOCKER_HOST = 'unix://var/run/docker.sock'
-
-KUBERNETES_CONFIG_PATH = '~/.kube/config'
